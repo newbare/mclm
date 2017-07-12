0236955132a00207d3cc624c3e47e3670fb2d798
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2017-07-12 14:33:31 -0300

    Correção do bug de Apagar Pasta. Implementação da troca da cor de fundo do mapa.

--- src/main/webapp/app/view/tools/ColorPickerWindow.js
+++ src/main/webapp/app/view/tools/ColorPickerWindow.js
@@ -6,33 +6,60 @@ Ext.define('MCLM.view.tools.ColorPickerWindow', {
 	xtype: 'colorPickerWindow',
 	title : "Seletor de Cor",
 	width : 368,
-	height: 210,
+	height: 240,
 	bodyStyle:"background:#FFFFFF;",
 	resizable: false,
 	constrain: true,
 	renderTo: Ext.getBody(),
 	
-	color : null,
-
     items: [{
         xtype: 'component',
         autoEl: 'div',
         id : 'colorpickerWHolder',
     }],
+    
+    oldColor : null,
+    currentColor : null,
+    colorComponent : null,
 
     init : function( initialColor, component ) {
     	var me = this;
+    	me.colorComponent = component;
+    	me.oldColor = initialColor;
+    	me.currentColor = initialColor;
     	
 		$('#colorpickerWHolder').ColorPicker({
 			color : initialColor,
 			flat: true,
 			onChange: function (hsb, hex, rgb) {
-				me.color = "#" + hex.toUpperCase();
-				component.setValue( me.color );
+				me.currentColor = "#" + hex.toUpperCase();
 			}
 		});
     	
     },
+    
+    buttons: [{
+        text: 'Padrão',
+        handler: function() {
+        	var colorPickerWindow = Ext.getCmp('colorPickerWindow');
+        	colorPickerWindow.colorComponent.setValue( '#EFF1F2' );
+        	colorPickerWindow.close();
+        }
+      },{
+        text: 'Cancelar',
+        handler: function() {
+        	var colorPickerWindow = Ext.getCmp('colorPickerWindow');
+        	colorPickerWindow.colorComponent.setValue( colorPickerWindow.oldColor );
+        	colorPickerWindow.close();
+        }
+      },{
+        text: 'Ok',
+        handler: function() {
+        	var colorPickerWindow = Ext.getCmp('colorPickerWindow');
+        	colorPickerWindow.colorComponent.setValue( colorPickerWindow.currentColor );
+        	colorPickerWindow.close();
+        }
+    }],    
     
     
     listeners: {
